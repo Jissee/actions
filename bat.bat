e2d83088258c666893371d2f93ee9a3fb28919f2
Jissee <50765103+Jissee@users.noreply.github.com>
2022-05-30 16:07:05 +0800

    Add files via upload

---
+++ bat.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Java\jdk1.8.0_301\jre\bin\java.exe" -jar -Dlog4j2.formatMsgNoLookups=true -Dlog4j.configurationFile=log4j2_112-116.xml arclight-forge-1.16.5-1.0.22-SNAPSHOT.jar
+pause
